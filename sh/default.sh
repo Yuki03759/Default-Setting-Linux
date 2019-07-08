1a63be0a732daabbdfc7a6fa40b88b51730e8034
“Yuki <yukih1@uci.edu>
2019-07-08 17:31:17 +0900

    sh file

---
+++ sh/default.sh
@@ -0,0 +1,9 @@
+# update & upgrade apt
+sudo apt update
+sudo apt upgrade
+
+# vim git tmux htop
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install tmux
+sudo apt-get install htop
---
+++ sh/script.sh
@@ -0,0 +1,7 @@
+chmod +x default.sh
+chmod +x rsa.sh
+chmod +x sf.sh
+
+sudo ./default.sh
+sudo ./rsa.sh
+sudo ./sf.sh
